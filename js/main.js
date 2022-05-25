9e608b8b16a827df8c16d806c72b7a0df05333a5
paco <pacojg@gmail.com>
2022-05-25 16:45:24 -0500

    Primer commit de algoritmos, checar los if y switch, hacer menú

---
+++ js/main.js
@@ -0,0 +1,51 @@
+// Usar promt, cosole log o alert
+// 1.- Solicitar 3 números (entre el 1 al 100) y definir cual es mayor
+// 2.- Solicitar 3 números (entre el 1 al 100) y definir cual es menor de tres números
+
+
+
+let num1 = prompt("Dame un número del 1 al 100", "");
+let num2 = prompt("Dame otro número del 1 al 100", "");
+let num3 = prompt("Dame otro número del 1 al 100", "");
+
+if (num1>num2) {
+     if (num1<num3) {
+        alert("El tercer número que me diste es el mayor");
+    }
+    else if(num1>num3)
+    alert("El primer número que me diste es el mayor");
+}
+
+
+
+
+
+
+
+
+
+
+/*switch (num1) {
+    case num1>num2:
+        switch (num1) {
+            case num1>num2:
+                alert("El número 1 es el mayor de todos");
+                break;
+        }
+        break;
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/main2.js
@@ -0,0 +1,8 @@
+let num1 = prompt("Dame un número del 100 al 200", "");
+
+function entero(num1){
+if(num1 / 3 == int)
+    alert("Este número es múltiplo de 3");
+} else {
+    alert("Este número no es múltiplo de 3");
+}
