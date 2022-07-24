13a8166d4a156e474f474b1481975ff80955a317
paco <pacojg.ti@gmail.com>
2022-07-24 17:27:24 -0500

    Se agregó el ejercicio 3

--- js/main.js
+++ js/main.js
@@ -66,7 +66,26 @@ switch (n){
             break;
         }
     case 3:
-        
+        function adivinar(){
+            let superior = 100;
+            let inferior = 0;
+            let noEncontrado = true;
+            while(noEncontrado){
+                let mid = parseInt( inferior + ((superior-inferior)/2));
+                if(((superior-inferior)/2)<1){
+                    noEncontrado = false;
+                    alert("Tu número es el "+(parseInt(mid)+1));
+                    break;
+                }
+                let res = confirm("Tu número es menor o igual a " + mid);
+                if(res){
+                    superior=mid;
+                }else{
+                    inferior=mid;
+                }
+            }
+        }
+        adivinar ();
     case 4:
         n1=parseFloat(prompt("Ingresa un número entre 100 y 200: ",""));
         res=(n1%3);
